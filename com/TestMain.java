4b8da7ef4256d7b18ec3a36fede2aa6ccd2d7e69
renzhanpo <renzhanpo@126.com>
2024-04-15 23:27:02 +0800

    again test it
    (cherry picked from commit dacbd96fee4b992b5b4ec68e142004926534872b)

--- com/TestMain.java
+++ com/TestMain.java
@@ -2,5 +2,6 @@ public class TestMain {
     public static void main(String[] args) {
         System.out.println("test");
         System.out.println("test2");
+        System.out.println("test3");
     }
 }
